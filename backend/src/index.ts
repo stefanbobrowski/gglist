a00549561cfedd8d1bbc0029ea0e2a21be2f4540
Stefan Bobrowski <stefanbobrowski1@gmail.com>
2025-06-22 22:22:16 -0500

    port mixup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ app.get("/", (_req: Request, res: Response) => {
   res.send("GGList API running");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`GGList Server running on port ${PORT}`);
 });
