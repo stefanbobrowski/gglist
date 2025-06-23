68b029434462686cfefaa32cd0b1c8e0a727c939
Stefan Bobrowski <stefanbobrowski1@gmail.com>
2025-06-22 22:12:44 -0500

    use 8080 port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,4 +7,7 @@ app.get("/", (_req: Request, res: Response) => {
   res.send("GGList API running");
 });
 
-app.listen(3000, () => console.log("GGList backend listening on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`GGList Server running on port ${PORT}`);
+});
