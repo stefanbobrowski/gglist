e8d508f37aab46ebeed75c99bc39f8ef6d54b275
Stefan Bobrowski <stefanbobrowski1@gmail.com>
2025-06-22 22:36:52 -0500

    vite config allowed hosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: "0.0.0.0",
+    port: 8080,
+    allowedHosts: ["gglist-frontend-177352903615.us-central1.run.app"],
+  },
+});
